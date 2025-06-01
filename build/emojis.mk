86bab85bcc15f2d33536be218df1ca13151cdf04
zawa-ch. <satellite.2e1834097@gmail.com>
2025-06-01 19:05:00 +0900

    Makefile作成

---
+++ build/emojis.mk
@@ -0,0 +1,13 @@
+.PHONY: emojis emojis.clean
+
+emojis: emojis.zip
+
+emojis.clean:
+	-rm emojis.zip .emojis.pre
+	-rm -rf emojis/
+
+.emojis.pre:
+	-mkdir -p emojis && \
+	touch .emojis.pre
+
+emojis.zip:
