888191699a1a16672655d6e6b608a424dc3aca1a
zawa-ch. <satellite.2e1834097@gmail.com>
2025-06-03 01:57:36 +0900

    メタデータのマージ自動化

--- build/emojis.mk
+++ build/emojis.mk
@@ -12,5 +12,5 @@ emojis.clean:
 
 emojis.zip: .emojis.pre barcode twemoji
 	cp -t emojis/ barcode/*.png twemoji/*.png
-	jq -nc --slurpfile barcode barcode/meta.json '{metaVersion:2,emojis:[]}|.emojis|=(.+$$barcode[0].emojis)' | jq -c --slurpfile twemoji twemoji/meta.json '.emojis|=(.+$$twemoji[0].emojis)' > emojis/meta.json
+	.script/merge_metadata.sh  barcode/meta.json twemoji/meta.json > emojis/meta.json
 	cd emojis/ && zip ../emojis.zip meta.json ./*.png
