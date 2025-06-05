304718a30558abb58fec4ee716c5824dfbc53068
zawa-ch. <satellite.2e1834097@gmail.com>
2025-06-06 02:37:57 +0900

    Makefile更新

--- build/blobs.mk
+++ build/blobs.mk
@@ -10,9 +10,6 @@ blobs.clean: blobs/blobbluebird.clean blobs/blobemoji.clean blobs/blobcat.clean 
 	-mkdir -p blobs && \
 	touch .blobs.pre
 
-blobs/catalogue.json: .blobs.pre
-	curl https://api.mousey.app/v3/emoji/blobs+community-blobs > blobs/catalogue.json
-
 include blobs.blobbluebird.mk
 include blobs.blobemoji.mk
 include blobs.blobcat.mk
