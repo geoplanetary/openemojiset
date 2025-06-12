cafa3cf464fcdf4b0bfc5c02d0851f51ab96de7e
zawa-ch. <satellite.2e1834097@gmail.com>
2025-06-12 18:32:42 +0900

    Barcode 個別ビルドとチェックの追加

--- build/barcode.mk
+++ build/barcode.mk
@@ -1,18 +1,32 @@
-.PHONY: barcode barcode.clean
+# Barcode
 
-barcode: \
-  barcode/meta.json \
-  barcode/ean13_4901777192607.png \
-  barcode/ean13_4902102136716.png \
-  barcode/ean13_4909411086299.png
-
-barcode.clean:
-	-rm -rf barcode/
-	-rm .barcode.pre
+barcode: barcode.zip
 
 .barcode.pre:
 	mkdir -p barcode
 	touch .barcode.pre
+
+barcode.clean:
+	-rm -rf barcode/
+	-rm barcode.zip
+	-rm .barcode.pre
+
+barcode.meta= barcode/meta.json
+
+barcode.meta: $(barcode.meta)
+
+barcode.assets= \
+  barcode/ean13_4901777192607.png \
+  barcode/ean13_4902102136716.png \
+  barcode/ean13_4909411086299.png
+
+barcode.assets: $(barcode.assets)
+
+barcode.check: $(barcode.meta) $(barcode.assets)
+	.script/check_metadata_integrity.sh barcode
+
+barcode.zip: $(barcode.meta) $(barcode.assets)
+	cd barcode && zip ../barcode.zip meta.json ./*.png
 
 barcode/meta.json: .barcode.pre ../barcode/meta.json
 	.script/build_metadata.sh ../barcode/meta.json > barcode/meta.json
@@ -20,3 +34,7 @@ barcode/meta.json: .barcode.pre ../barcode/meta.json
 barcode/%.png: ../barcode/ean/%.svg .barcode.pre
 	resvg -z 4.0 --dpi 384 "$<" "$@"
 	optipng -q --fix "$@"
+
+# ------------------------------------ #
+
+.PHONY: barcode barcode.clean
