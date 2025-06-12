449a0c140313059b86a6e16840746965b509349b
zawa-ch. <satellite.2e1834097@gmail.com>
2025-06-12 18:48:40 +0900

    Twemoji 個別ビルドとチェックの追加

--- build/twemoji.mk
+++ build/twemoji.mk
@@ -1,7 +1,21 @@
-.PHONY: twemoji twemoji.clean
+# Twemoji
 
-twemoji: \
-  twemoji/meta.json \
+twemoji: twemoji.zip
+
+.twemoji.pre:
+	mkdir -p twemoji
+	touch .twemoji.pre
+
+twemoji.clean:
+	-rm -rf twemoji/
+	-rm twemoji.zip
+	-rm .twemoji.pre
+
+twemoji.meta= twemoji/meta.json
+
+twemoji.meta: $(twemoji.meta)
+
+twemoji.assets= \
   twemoji/squared_16.png \
   twemoji/squared_32.png \
   twemoji/squared_32.png \
@@ -19,13 +33,13 @@ twemoji: \
   twemoji/squared_32768.png \
   twemoji/squared_65536.png
 
-twemoji.clean:
-	-rm -rf twemoji/
-	-rm .twemoji.pre
+twemoji.assets: $(twemoji.assets)
 
-.twemoji.pre:
-	mkdir -p twemoji
-	touch .twemoji.pre
+twemoji.check: $(twemoji.meta) $(twemoji.assets)
+	.script/check_metadata_integrity.sh twemoji
+
+twemoji.zip: $(twemoji.meta) $(twemoji.assets)
+	cd twemoji && zip ../twemoji.zip meta.json ./*.png
 
 twemoji/meta.json: .twemoji.pre ../twemoji/meta.json
 	.script/build_metadata.sh ../twemoji/meta.json > twemoji/meta.json
@@ -33,3 +47,7 @@ twemoji/meta.json: .twemoji.pre ../twemoji/meta.json
 twemoji/%.png: ../twemoji/%.svg .twemoji.pre
 	resvg -z 4.0 --dpi 384 "$<" "$@"
 	optipng -q "$@"
+
+# ------------------------------------ #
+
+.PHONY: twemoji twemoji.clean twemoji.meta twemoji.assets twemoji.check
